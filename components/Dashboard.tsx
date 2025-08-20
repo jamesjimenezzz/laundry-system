be88878bdc77d0a60c608532a8ec62b4d3b29097
james jimenez <asunahaley@gmail.com>
2025-08-20 18:48:53 +0800

    improved dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -8,20 +8,53 @@ import {
 } from "./ui/card";
 
 const Dashboard = () => {
+  const dashboardData = [
+    {
+      title: "Todays Customers",
+      value: 12,
+      gradient: "from-blue-600 to-indigo-400",
+    },
+    {
+      title: "Waiting Orders",
+      value: 12,
+      gradient: "from-yellow-500 to-amber-300",
+    },
+    {
+      title: "On Process Orders",
+      value: 12,
+      gradient: "from-purple-500 to-pink-300",
+    },
+    {
+      title: "Done Orders",
+      value: 12,
+      gradient: "from-emerald-600 to-green-400",
+    },
+    {
+      title: "Gross Income",
+      value: 12,
+      gradient: "from-orange-500 to-red-300",
+    },
+    {
+      title: "Approximate Net Income",
+      value: 12,
+      gradient: "from-teal-600 to-cyan-400",
+    },
+  ];
+
   return (
     <div className="grid grid-cols-3 gap-6">
-      {Array.from({ length: 6 }, (_, i) => (
-        <Card className="gap-2" key={i}>
+      {dashboardData.map((d, i) => (
+        <Card className={`bg-gradient-to-br ${d.gradient}`} key={i}>
           <CardHeader>
             <CardTitle>
-              <h1 className="text-xl">Today Customers</h1>
+              <h1 className="text-2xl font-bold text-white">{d.title}</h1>
             </CardTitle>
           </CardHeader>
           <CardContent>
             <CardDescription className="flex flex-col gap-2">
-              <p className="font-bold text-3xl">12</p>
-              <p className="text-sm">
-                <span className="bg-gray-200 rounded-lg py-0.5 px-2 font-semibold">
+              <p className="font-bold text-3xl text-white">{d.value}</p>
+              <p className="text-sm text-white ">
+                <span className="bg-gray-200 text-gray-500 rounded-lg py-0.5 px-2 font-semibold">
                   +4
                 </span>{" "}
                 vs yesterday
