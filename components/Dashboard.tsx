0eddaf5366c690d3ce7164b4333bf502f479b133
james jimenez <asunahaley@gmail.com>
2025-08-20 23:08:13 +0800

    added dynamic data on dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Card,
@@ -6,45 +7,67 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
+import { useCustomers } from "@/hooks/useCustomer";
+import { Customer } from "@prisma/client";
 
 const Dashboard = () => {
+  const { data } = useCustomers({ preset: "today" });
+
+  const todayCustomers = data?.length;
+  const waitingOrders = data?.filter(
+    (d: Customer) => d.status === "Waiting"
+  ).length;
+  const processingOrders = data?.filter(
+    (d: Customer) => d.status === "OnProcess"
+  ).length;
+  const doneOrders = data?.filter((d: Customer) => d.status === "Done").length;
+  const grossIncome = data?.reduce(
+    (acc: number, d: Customer) => acc + d.price,
+    0
+  );
+  const todayLoads = data?.reduce(
+    (acc: number, d: Customer) => acc + d.loads,
+    0
+  );
+
   const dashboardData = [
     {
       title: "Todays Customers",
-      value: 12,
+      value: todayCustomers,
       gradient: "from-blue-600 to-indigo-400",
     },
     {
+      title: "Todays Loads",
+      value: todayLoads,
+      gradient: "from-teal-600 to-cyan-400",
+    },
+    {
+      title: "Gross Income",
+      value: "₱" + " " + grossIncome,
+      gradient: "from-orange-500 to-red-300",
+    },
+
+    {
       title: "Waiting Orders",
-      value: 12,
+      value: waitingOrders,
       gradient: "from-yellow-500 to-amber-300",
     },
     {
       title: "On Process Orders",
-      value: 12,
+      value: processingOrders,
       gradient: "from-purple-500 to-pink-300",
     },
     {
       title: "Done Orders",
-      value: 12,
+      value: doneOrders,
       gradient: "from-emerald-600 to-green-400",
-    },
-    {
-      title: "Gross Income",
-      value: 12,
-      gradient: "from-orange-500 to-red-300",
-    },
-    {
-      title: "Approximate Net Income",
-      value: 12,
-      gradient: "from-teal-600 to-cyan-400",
     },
   ];
 
   return (
     <div className="grid grid-cols-3 gap-6">
       {dashboardData.map((d, i) => (
-        <Card className={`bg-gradient-to-br ${d.gradient}`} key={i}>
+        <Card className={`bg-gradient-to-br ${d.gradient} py-10`} key={i}>
           <CardHeader>
             <CardTitle>
               <h1 className="text-2xl font-bold text-white">{d.title}</h1>
