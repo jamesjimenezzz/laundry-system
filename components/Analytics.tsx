20ff2d881c1d27edee908547864d64e6151f9505
james jimenez <asunahaley@gmail.com>
2025-08-20 16:42:50 +0800

    added gradients

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,10 +1,62 @@
 import React from "react";
 import ChartLineLabel from "./LineChart";
 import AnalyticsHeading from "./ui/analyticsheading";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./ui/card";
+
 const Analytics = () => {
+  const analyzeCards = [
+    {
+      name: "Total Customers",
+      value: 80,
+      description: "In selected period",
+      gradient: "from-blue-500 to-blue-300",
+    },
+    {
+      name: "Total Income",
+      value: "16,000",
+      description: "In selected period",
+      gradient: "from-green-500 to-green-300",
+    },
+    {
+      name: "Daily Average",
+      value: 11,
+      description: "Customers per day",
+      gradient: "from-orange-500 to-orange-300",
+    },
+    {
+      name: "Average Order",
+      value: 200,
+      description: "Per Customer",
+      gradient: "from-purple-500 to-purple-300",
+    },
+  ];
+
   return (
     <div>
       <AnalyticsHeading />
+      <div className="grid grid-cols-4 gap-5 mb-10">
+        {analyzeCards.map((ac, i) => (
+          <Card className={`bg-gradient-to-br ${ac.gradient} py-9`} key={i}>
+            <CardHeader>
+              <CardTitle className="text-white font-semibold">
+                {ac.name}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>
+                <p className="text-white">{ac.value}</p>
+                <p className="text-white">{ac.description}</p>
+              </CardDescription>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
       <ChartLineLabel />
     </div>
   );
--- components/ui/analyticsheading.tsx
+++ components/ui/analyticsheading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AnalyticsHeading = () => {
   return (
-    <div className="bg-blue-500 rounded-lg mb-10 flex flex-col gap-2 px-5 py-5 w-full">
+    <div className="bg-gradient-to-tr from-blue-500 to-blue-300 rounded-lg mb-10 flex flex-col gap-2 px-5 py-5 w-full">
       <div className="flex items-center gap-2 ">
         <div className="p-2 rounded-lg text-white  bg-white/30">
           <LineChart className="" size={20} />
